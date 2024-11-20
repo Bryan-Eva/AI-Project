1ca4ab8602e829a86e08040b088ccbceac01b7a9
BryanJhuo <bryan86874928@gmail.com>
2024-11-21 01:30:47 +0800

    update req. & add homepage

---
+++ main.py
@@ -0,0 +1,14 @@
+def CreateQTwindow():
+    return 0
+
+def main():
+    return 0
+
+if "__main__" == __name__:
+    # TODO: 完成RAG training
+    # TODO: PyQt- homepage
+    # TODO: PyQt- coursepage
+    # TODO: PyQt- questionpage
+    # TODO: PyQt- testingpage
+    CreateQTwindow()
+    main()
